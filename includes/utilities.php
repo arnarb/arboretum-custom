29b326469cfdd283ac44bb6dbc04c0506776f416
MatthewCaulkins <matt.caulkins@gmail.com>
2022-11-01 16:31:09 -0400

    Adding images and get algolia util function

--- includes/utilities.php
+++ includes/utilities.php
@@ -444,4 +444,15 @@ function get_solar_data($atts) {
     echo $output;
 }
 add_shortcode('get_solar_data', 'get_solar_data');
-  
+
+
+/**
+ * Pulls in the Algolia json file and returns it
+ */
+function get_algolia_data() {
+    $filename = $_SERVER['DOCUMENT_ROOT'] . '/algolia_calc.json';
+    $file = file_get_contents($filename);
+    $json = json_decode($file);
+
+    return $json;
+}
