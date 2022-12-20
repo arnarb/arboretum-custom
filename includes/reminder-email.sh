1c2ef0704d2f09fcd469058d957be0d7d65d43f9
Matthew Caulkins <matthew_caulkins@harvard.edu>
2022-12-20 08:14:35 -0500

    Email reminders script

--- includes/reminder-email.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Filename: reminder-email.sh
-#Description: Send reminder emails for events
-
-/usr/bin/php /www/arnoldarboretumwebsite_753/public/wp-content/plugins/arboretum-custom/includes/reminder-emails.php
